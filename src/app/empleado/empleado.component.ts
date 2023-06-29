eb57152e53f5578b1fe95180857547fd41c62b69
Joshua Daniel <joshua_dup@hotmail.com>
2023-06-29 17:52:04 -0500

    texto dinamico usando interpolacion

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.scss']
+  styleUrls: ['./empleado.component.scss'],
 })
 export class EmpleadoComponent {
+  nombre = 'Luis';
+  apellido = 'Díaz';
+  edad = 25;
+  // private edad = 25;
+  empresa = 'Liverpool';
 
+  // getEdad() {
+  //   return this.edad;
+  // }
 }
+
+// Puedo agregar un modificacion de acceso private para que solamente pueda ser leido desde la misma clase
+// Podemos crear un metodoo get para acceder al valor de una propiedad privada
